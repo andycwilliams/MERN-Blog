5241212925662eec3ff2a933d7e7568a4ee1d378
Andy <awilliamscomposing@gmail.com>
2024-12-09 21:19:59 -0800

    Adding component styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../images/thumbnail.jpg";
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <ul className="footer__categories">
+        <li>
+          <Link to="posts/categories/:Agriculture">Agriculture</Link>
+        </li>
+        <li>
+          <Link to="posts/categories/:Business">Business</Link>
+        </li>
+        <li>
+          <Link to="posts/categories/:Education">Education</Link>
+        </li>
+        <li>
+          <Link to="posts/categories/:Entertainment">Entertainment</Link>
+        </li>
+        <li>
+          <Link to="posts/categories/:Art">Art</Link>
+        </li>
+        <li>
+          <Link to="posts/categories/:Investment">Investment</Link>
+        </li>
+        <li>
+          <Link to="posts/categories/:Uncategorized">Uncategorized</Link>
+        </li>
+        <li>
+          <Link to="posts/categories/:Weather">Weather</Link>
+        </li>
+      </ul>
+      <div className="footer__copyright">
+        <small>All Rights Reserved &copy; Copyright, EGATOR Tutorials.</small>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
