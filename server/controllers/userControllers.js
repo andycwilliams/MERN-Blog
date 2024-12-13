3196ba3ac015e9a51e81944cd87aade0a11eb248
Andy <awilliamscomposing@gmail.com>
2024-12-12 17:26:39 -0800

    Adding more user routes

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,49 +1,225 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const fs = require("fs");
+const path = require("path");
+const { v4: uuid } = require("uuid");
+
+const User = require("../models/userModel");
+const HttpError = require("../models/errorModel");
+
 // ========================= REGISTER NEW USER =========================
 
 // POST: api/users/register
 // UNPROTECTED
-const registerUser = (req, res, next) => {
-  res.json("Registered user");
+const registerUser = async (req, res, next) => {
+  // res.json("Registered user");
+
+  try {
+    const { name, email, password, password2 } = req.body;
+    if (!name || !email || !password) {
+      return next(new HttpError("Please fill in all fields.", 422));
+    }
+
+    const newEmail = email.toLowerCase();
+
+    const emailExists = await User.findOne({ email: newEmail });
+    if (emailExists) {
+      return next(new HttpError("Email already exists.", 422));
+    }
+
+    if (password.trim().length < 6) {
+      return next(
+        new HttpError("Password should be at least 6 characters."),
+        422
+      );
+    }
+
+    if (password != password2) {
+      return next(new HttpError("Passwords do not match.", 422));
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = await User.create({
+      name,
+      email: newEmail,
+      password: hashedPassword,
+    });
+    res.status(201).json(`New User ${newUser.email} registered!`);
+  } catch (error) {
+    return next(new HttpError("User registration failed.", 422));
+  }
 };
 
 // ========================= LOG IN =========================
 
 // POST: api/users/login
 // UNPROTECTED
-const loginUser = (req, res, next) => {
-  res.json("Login user");
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return next(new HttpError("Please fill in all fields.", 422));
+    }
+    const newEmail = email.toLowerCase();
+
+    const user = await User.findOne({ email: newEmail });
+    if (!user) {
+      return next(new HttpError("Invalid credentials.", 422));
+    }
+
+    const comparePassword = await bcrypt.compare(password, user.password);
+    if (!comparePassword) {
+      return next(new HttpError("Invalid credentials.", 422));
+    }
+
+    const { _id: id, name } = user;
+    const token = jwt.sign({ id, name }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(200).json({ token, id, name });
+  } catch (error) {
+    return next(
+      new HttpError("Login failed. Please check your credentials.", 422)
+    );
+  }
 };
 
 // ========================= USER PROFILE =========================
 
 // POST: api/users/:id
 // PROTECTED
-const getUser = (req, res, next) => {
-  res.json("User profile");
+const getUser = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id).select("-password");
+    if (!user) {
+      return next(new HttpError("User not found.", 400));
+    }
+    res.status(202).json(user);
+  } catch (error) {
+    return next(new HttpError(error));
+  }
 };
 
 // ========================= CHANGE USER AVATAR (profile picture) =========================
 
 // POST: api/users/change-avatar
 // PROTECTED
-const changeAvatar = (req, res, next) => {
-  res.json("Change user avatar");
+const changeAvatar = async (req, res, next) => {
+  try {
+    if (!req.files.avatar) {
+      return next(new HttpError("Please select an image.", 422));
+    }
+
+    const user = await User.findById(req.user.id);
+    if (user.avatar) {
+      fs.unlink(path.join(__dirname, "..", "uploads", user.avatar), (err) => {
+        if (err) {
+          return next(new HttpError(err));
+        }
+      });
+    }
+
+    const { avatar } = req.files;
+    if (avatar.size > 500000) {
+      return next(
+        new HttpError("Profile picture should be less than 500kb.", 422)
+      );
+    }
+
+    let fileName;
+    fileName = avatar.name;
+    let splitFileName = fileName.split(".");
+    let newFileName =
+      splitFileName[0] + uuid() + "." + splitFileName[splitFileName.length - 1];
+    avatar.mv(
+      path.join(__dirname, "..", "uploads", newFileName),
+      async (err) => {
+        if (err) {
+          return next(new HttpError(error));
+        }
+
+        const updatedAvatar = await User.findByIdAndUpdate(
+          req.user.id,
+          { avatar: newFileName },
+          { new: true }
+        );
+        if (!updatedAvatar) {
+          return next(new HttpError("Avatar couldn't be changed.", 422));
+        }
+        res.status(200).json(updatedAvatar);
+      }
+    );
+  } catch (error) {
+    return next(new HttpError(error));
+  }
 };
 
 // ========================= EDIT USER DETAILS (from profile) =========================
 
 // POST: api/users/edit-user
 // PROTECTED
-const editUser = (req, res, next) => {
-  res.json("Edit user details");
+const editUser = async (req, res, next) => {
+  try {
+    const { name, email, currentPassword, newPassword, newConfirmNewPassword } =
+      req.body;
+    if (!name || !email || currentPassword || !newPassword) {
+      return next(new HttpError("Please fill in all fields.", 422));
+    }
+
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      return next(new HttpError("User not found", 403));
+    }
+
+    const emailExists = await User.findOne({ email });
+    if (emailExists && emailExists._id != req.user.id) {
+      return next(new HttpError("Email already exists.", 422));
+    }
+
+    const validateUserPassword = await bcrypt.compare(
+      currentPassword,
+      user.password
+    );
+    if (!validateUserPassword) {
+      return next(new HttpError("Invalid current password", 422));
+    }
+
+    if (newPassword !== newConfirmNewPassword) {
+      return next(new HttpError("New passwords do not match.", 422));
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+    const newInfo = await User.findByIdAndUpdate(
+      req.user.id,
+      {
+        name,
+        email,
+        password: hashedPassword,
+      },
+      { new: true }
+    );
+    res.status(200).json(newInfo);
+  } catch (error) {
+    return next(new HttpError(error));
+  }
 };
 
 // ========================= GET ALL USERS =========================
 
 // POST: api/users/authors
 // UNPROTECTED
-const getAuthors = (req, res, next) => {
-  res.json("Get all users/authors");
+const getAuthors = async (req, res, next) => {
+  try {
+    const authors = await User.find().select("-password");
+    res.json(authors);
+  } catch (error) {
+    return next(new HttpError(error));
+  }
 };
 
 module.exports = {
